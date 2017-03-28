8ebebdee8517c86e366c03be94ff9ef19c524f7d
Avic <Avicennasis@gmail.com>
2017-03-27 22:52:42 -0400

    Update dyndns.sh
    Double quotes to prevent globbing and word splitting.

--- dyndns.sh
+++ dyndns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Get external IP and send it to remote server
-curl http://icanhazip.com >${HOME}/.dyndns/homeip
-rsync -a ${HOME}/.dyndns/homeip USERNAME@HOST.COM:/etc/bind/zones
+curl http://icanhazip.com >"${HOME}/.dyndns/homeip"
+rsync -a "${HOME}/.dyndns/homeip" USERNAME@HOST.COM:/etc/bind/zones
